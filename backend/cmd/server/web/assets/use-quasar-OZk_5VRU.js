c2bdb7f34c303f5c3a2469a54113124d8b13dd70
Карташов Антон Алексеевич <akart@ftm.su>
2026-03-12 20:06:58 +0300

    feat: добавлен начальный сайт

---
+++ backend/cmd/server/web/assets/use-quasar-OZk_5VRU.js
@@ -0,0 +1,1 @@
+import{N as a,aV as r}from"./index-CT6VCLMC.js";function u(){return a(r)}export{u};
--- backend/cmd/server/web/assets/use-quasar-So7HNuyi.js
+++
@@ -1,1 +0,0 @@
-import{N as a,aV as r}from"./index-BBHnjVri.js";function u(){return a(r)}export{u};
