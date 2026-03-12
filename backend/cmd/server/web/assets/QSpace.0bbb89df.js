af2f9b707c4edce265fb2be3d1c8c29e42cdeb5d
Карташов Антон Алексеевич <akart@ftm.su>
2026-03-12 18:48:19 +0300

    feat: add site debug

---
+++ backend/cmd/server/web/assets/QSpace.0bbb89df.js
@@ -0,0 +1,1 @@
+import{h as e,H as s}from"./index.0f401156.js";var c=e({name:"QSpace",setup(){const a=s("div",{class:"q-space"});return()=>a}});export{c as Q};
---
+++ backend/cmd/server/web/assets/use-quasar.35b2c0a2.js
@@ -0,0 +1,1 @@
+import{N as a,aM as r}from"./index.0f401156.js";function u(){return a(r)}export{u};
